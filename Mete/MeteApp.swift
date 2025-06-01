0296ea2581e1745a80b80837045a8f1f2a7d5032
CHWAAN <nebo3521@gmail.com>
2025-06-01 21:14:43 +0900

    Initial commit

---
+++ Mete/MeteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteApp.swift
+//  Mete
+//
+//  Created by Mark on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MeteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeteTests/MeteTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeteTests.swift
+//  MeteTests
+//
+//  Created by Mark on 6/1/25.
+//
+
+import Testing
+@testable import Mete
+
+struct MeteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
